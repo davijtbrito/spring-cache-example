651fc3345c886a958df89fb7b7419d6e2a62d215
davibrito1218 <davibrito1218@gmail.com>
2023-01-24 17:05:49 +0000

    first commit

---
+++ spring-cache-example/src/test/java/com/cache/springcacheexample/SpringCacheExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cache.springcacheexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
