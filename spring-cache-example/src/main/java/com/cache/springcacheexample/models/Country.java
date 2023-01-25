655d1f1a68316ca87858ea6c859c0a3bf2674309
davibrito1218 <davibrito1218@gmail.com>
2023-01-25 03:02:38 +0000

    Fixing reference

--- spring-cache-example/src/main/java/com/cache/springcacheexample/models/Country.java
+++ spring-cache-example/src/main/java/com/cache/springcacheexample/models/Country.java
@@ -1,7 +1,5 @@
 package com.cache.springcacheexample.models;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
